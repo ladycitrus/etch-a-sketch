a1c707a625a3db24e3932664ee1cf01069abd9e5
Annica <annicarberg@gmail.com>
2024-10-27 11:08:56 +0900

    Add createRows function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,28 @@
 // Sets important constants and variables
-const container = document.querySelector('container');
+const container = document.querySelector('.container');
+
+// Create a function to make Grid
+
+//function createGrid() {
+
+    // create rows function
+    function createRows(rowNum) {
+        for (r = 0; r < 16; r++) {
+            const row = document.createElement('div');
+            row.innerText = r + 1;
+            container.appendChild(row);
+        }
+    }
+
+    
+    // create columns
+
+    function createColumns(colNum) {
+        
+    
+    }
+
+//}
 
 // Creates a default grid sized 16x16
 function defaultGrid(rowNum, colNum) {
@@ -7,21 +30,7 @@ function defaultGrid(rowNum, colNum) {
     createColumns(16);
 }
 
-// Create a function to make Grid
-
-function createGrid() {
-
-    // create rows
-    function createRows(rowNum) {
-    }
-
-    // create columns
-
-    function createColumns(colNum) {
-    
-    }
-
-}
-
 
 // call function
+
+createRows(16);
